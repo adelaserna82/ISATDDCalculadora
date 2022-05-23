5a94fc218ea5067f38cb2539b3651cd6339f21e8
Alberto de la Serna Parada <adelaserna82@gmail.com>
2022-05-23 21:25:51 +0200

    Acabo de implementar en la clase Calculadora el método restar. Una vez implementado, he realizado la prueba unitaria de la resta y lo ha pasado satisfactoriamente.
    Para comprobar que lo desarrollado con anterioridad ha funcionado bien, he pasado todas las pruebas unitarias (testSuma y testResta) y las ha pasado satisfactoriamente.

--- src/com/calculadora/Calculadora.java
+++ src/com/calculadora/Calculadora.java
@@ -4,4 +4,8 @@ public class Calculadora {
     public float sumar(float numero1, float numero2) {
         return numero1 + numero2;
     }
+
+    public float restar(float numero1, float numero2) {
+        return numero1 - numero2;
+    }
 }
