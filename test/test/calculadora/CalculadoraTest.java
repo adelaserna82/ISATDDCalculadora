d666bbf421ab5c3971d3f1388bd450fe06ffe404
Josu Abejon <josuabejon@gmail.com>
2022-05-24 17:43:34 +0200

    test de multiplicar y dividir añadidos

--- test/test/calculadora/CalculadoraTest.java
+++ test/test/calculadora/CalculadoraTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Escribir un programa que realice operaciones aritméticas básicas
- * de una calculadora (suma, resta, multiplicación, raíz cuadrada).
+ * de una calculadora (suma, resta, multiplicación, división, raíz cuadrada).
  */
 public class CalculadoraTest {
 
@@ -42,6 +42,40 @@ public class CalculadoraTest {
         float numero2 = 2.0f;
         // act
         float resultadoObtenido = c.restar(numero1, numero2);
+        // assert
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+        /**
+     * Tenemos que tener un método de la clase calculadora que tiene que multiplicar dos números reales
+     * representados como float
+     */
+    @Test
+    public  void testMultiplicacion() {
+        // arrange
+        Calculadora c = new Calculadora();
+        float resultadoEsperado = 6.0f;
+        float numero1 = 3.0f;
+        float numero2 = 2.0f;
+        // act
+        float resultadoObtenido = c.multipicar(numero1, numero2);
+        // assert
+        assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+        /**
+     * Tenemos que tener un método de la clase calculadora que tiene que dividir dos números reales
+     * representados como float
+     */
+    @Test
+    public  void testDivision() {
+        // arrange
+        Calculadora c = new Calculadora();
+        float resultadoEsperado = 1.5f;
+        float numero1 = 3.0f;
+        float numero2 = 2.0f;
+        // act
+        float resultadoObtenido = c.dividir(numero1, numero2);
         // assert
         assertEquals(resultadoEsperado, resultadoObtenido);
     }
