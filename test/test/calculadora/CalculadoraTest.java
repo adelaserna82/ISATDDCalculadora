916035051bdb66f05cc0a2af25988685b3462c8c
Alberto de la Serna Parada <adelaserna82@gmail.com>
2022-06-01 23:00:39 +0200

    Para todos los métodos test (suma, resta, multiplicación, raiz cuadrada) he creado varias pruebas en cada test. He creado una clase Mock para cargar la información de cada prueba y después recorro el array y compruebo resultados esperados.

--- test/test/calculadora/CalculadoraTest.java
+++ test/test/calculadora/CalculadoraTest.java
@@ -19,33 +19,68 @@ public class CalculadoraTest {
     */
     @Test
     public void testSuma() {
+
         // arrange
+        class Mock {
+            protected float numero1;
+            protected float numero2;
+            protected float resultadoEsperado;
+            public Mock(float numero1, float numero2, float resultadoEsperado) {
+                this.numero1 = numero1;
+                this.numero2 = numero2;
+                this.resultadoEsperado = resultadoEsperado;
+            }
+        }
+
+        Mock[] tests = {
+                new Mock(2.0f, 3.0f,5.0f),
+                new Mock(-2.0f, 3.0f, 1.0f),
+                new Mock(2.0f,-3.0f,-1.0f),
+        };
         Calculadora c = new Calculadora();
-        float resultadoEsperado = 5.0f;
-        float numero1 = 2.0f;
-        float numero2 = 3.0f;
-        // act
-        float resultadoObtenido = c.sumar(numero1, numero2);
-        // assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
+
+        for(Mock test : tests){
+            // act
+            float resultadoObtenido = c.sumar(test.numero1, test.numero2);
+            // assert
+            assertEquals(test.resultadoEsperado, resultadoObtenido);
+        }
+
     }
+
 
     /**
      * Tenemos que tener un método de la clase calculadora que tiene que restar dos números reales
      * representados como float
      */
     @Test
-    public  void testResta() {
-        // arrange
-        Calculadora c = new Calculadora();
-        float resultadoEsperado = 1.0f;
-        float numero1 = 3.0f;
-        float numero2 = 2.0f;
-        // act
-        float resultadoObtenido = c.restar(numero1, numero2);
-        // assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
-    }
+    public void testResta() {
+            // arrange
+            class Mock {
+                protected float numero1;
+                protected float numero2;
+                protected float resultadoEsperado;
+                public Mock(float numero1, float numero2, float resultadoEsperado) {
+                    this.numero1 = numero1;
+                    this.numero2 = numero2;
+                    this.resultadoEsperado = resultadoEsperado;
+                }
+            }
+
+            Mock[] tests = {
+                    new Mock(2.0f, 3.0f,-1.0f),
+                    new Mock(-2.0f, 3.0f, -5.0f),
+                    new Mock(2.0f,-3.0f,5.0f),
+            };
+            Calculadora c = new Calculadora();
+
+            for(Mock test : tests){
+                // act
+                float resultadoObtenido = c.restar(test.numero1, test.numero2);
+                // assert
+                assertEquals(test.resultadoEsperado, resultadoObtenido);
+            }
+        }
 
         /**
      * Tenemos que tener un método de la clase calculadora que tiene que multiplicar dos números reales
@@ -54,14 +89,30 @@ public class CalculadoraTest {
     @Test
     public  void testMultiplicacion() {
         // arrange
+        class Mock {
+            protected float numero1;
+            protected float numero2;
+            protected float resultadoEsperado;
+            public Mock(float numero1, float numero2, float resultadoEsperado) {
+                this.numero1 = numero1;
+                this.numero2 = numero2;
+                this.resultadoEsperado = resultadoEsperado;
+            }
+        }
+
+        Mock[] tests = {
+                new Mock(2.0f, 3.0f,6.0f),
+                new Mock(-2.0f, 3.0f, -6.0f),
+                new Mock(2.0f,-3.0f,-6.0f),
+        };
         Calculadora c = new Calculadora();
-        float resultadoEsperado = 6.0f;
-        float numero1 = 3.0f;
-        float numero2 = 2.0f;
-        // act
-        float resultadoObtenido = c.multipicar(numero1, numero2);
-        // assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
+
+        for(Mock test : tests){
+            // act
+            float resultadoObtenido = c.multipicar(test.numero1, test.numero2);
+            // assert
+            assertEquals(test.resultadoEsperado, resultadoObtenido);
+        }
     }
 
         /**
@@ -71,15 +122,40 @@ public class CalculadoraTest {
     @Test
     public  void testDivision() {
         // arrange
+        class Mock {
+            protected float numero1;
+            protected float numero2;
+            protected float resultadoEsperado;
+            public Mock(float numero1, float numero2, float resultadoEsperado) {
+                this.numero1 = numero1;
+                this.numero2 = numero2;
+                this.resultadoEsperado = resultadoEsperado;
+            }
+        }
+
+        Mock[] tests = {
+                new Mock(3.0f, 2.0f,1.5f),
+                new Mock(2.0f, 2.0f, 1.0f),
+                new Mock(10.0f,2.0f,5.0f),
+        };
         Calculadora c = new Calculadora();
-        float resultadoEsperado = 1.5f;
-        float numero1 = 3.0f;
-        float numero2 = 2.0f;
-        // act
-        float resultadoObtenido = c.dividir(numero1, numero2);
-        // assert
-        assertEquals(resultadoEsperado, resultadoObtenido);
+
+        for(Mock test : tests){
+            try {
+
+                // act
+                float resultadoObtenido = c.dividir(test.numero1, test.numero2);
+                // assert
+                assertEquals(test.resultadoEsperado, resultadoObtenido);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
+
+
     /**
      * Tenemos que tener un método de la clase calculadora que tiene que realizar
      * la raíz cuadrada de un número real representado como float
@@ -87,13 +163,33 @@ public class CalculadoraTest {
     @Test
     public  void testRaizCuadrada() {
         // arrange
+        class Mock {
+            protected float numero;
+            protected float resultadoEsperado;
+            public Mock(float numero,  float resultadoEsperado) {
+                this.numero = numero;
+                this.resultadoEsperado = resultadoEsperado;
+            }
+        }
+
+        Mock[] tests = {
+                new Mock(45.0f, 6.708203932499369f),
+                new Mock(15.0f,  3.872983346207417f)
+        };
         Calculadora c = new Calculadora();
-        float resultadoEsperado = 6.708203932499369f;
-        float numero1 = 45.0f;
-        // act
-        float resultadoObtenido = c.raizCuadrada(numero1);
-        // assert
-        assertTrue(resultadoEsperado - resultadoObtenido < 0.00001);
+
+        for(Mock test : tests){
+            try {
+                // act
+                float resultadoObtenido = c.raizCuadrada(test.numero);
+                // assert
+                assertTrue(test.resultadoEsperado - resultadoObtenido < 0.00001);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
 }
